868323ac2bf06c850985280285c3af2e38b77b16
Eleanore Clark <78524415+eleclarkpdx@users.noreply.github.com>
2022-02-27 21:21:45 -0800

    All cards now have info from the Dogs API.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
-let row = document.querySelector(".row");
+const row = document.querySelector(".row");
 
-//input event
+const capitalizeFirst = (toCapitalize) =>
+{
+  return(toCapitalize[0].toUpperCase() + toCapitalize.slice(1));
+}
+
+const finalizeName = (breedName) => {
+ return (tryAddDogSuffix(fixName(breedName)));
+}
+
+const tryAddDogSuffix = (breedName) => {
+  str = breedName.toLowerCase();
+  if (str.includes("hound") || str.includes("hund")
+    || str.includes("dog"))
+    return breedName;
+  else 
+    return (breedName + " Dog");
+}
+
+const fixName = (breedName) => {
+  switch (breedName) {
+    case "Shepherd Australian":
+      breedName = "Australian Shepherd";
+      break;
+    case "Lapphund Finnish":
+      breedName = "Finnish Lapphund";
+      break;
+    case "Mix":
+      breedName = "Mixed";
+      break;
+    default:
+      break;
+  }
+  return breedName;
+}
 
 //creates a card as dog breed is being searched
 const createCard = (url, breedName, breedInfo) => {
@@ -14,7 +47,14 @@ const createCard = (url, breedName, breedInfo) => {
   card.setAttribute("class", "card");
   image.setAttribute("src", `${url}`);
   image.setAttribute("class", "card-img-top");
-  image.setAttribute("alt", `Image of ${breedName}`);
+  switch (breedName[0]){
+    case ("A" || "E" || "I" || "O" || "U"):
+      image.setAttribute("alt", `An ${breedName}`);
+      break;
+    default:
+      image.setAttribute("alt", `A ${breedName}`);
+      break;
+  }
   cardBody.setAttribute("class", "card-body");
   cardTitle.setAttribute("class", "card-title");
 
@@ -29,13 +69,48 @@ const createCard = (url, breedName, breedInfo) => {
   return cardSize;
 };
 
-let cards = createCard(
-  "https://place.dog/400/400",
-  "golden retriever",
-  `Lorem ipsum dolor sit amet consectetur adipisicing elit
-								Molestias alias error ex a repellat in est perferendis
-								commodi quam temporibus consectetur maiores unde sit
-								expedita distinctio, mollitia officiis ipsum? Quidem!`
-);
+const createAllCards = async () => {
+    const allBreedsResponse = (await fetch('https://dog.ceo/api/breeds/list/all'));
+    const allBreedsObj = (await allBreedsResponse.json()).message;
+    const breedNames = [];
+    const breedImgsSrcs = [];
 
-row.append(cards);
+    console.log(allBreedsObj);
+    for (breed in allBreedsObj) {
+      let breedName = capitalizeFirst(breed);
+      let breedImg = "https://dog.ceo/api/breed/";
+
+      if (allBreedsObj[breed].length == 0) {
+        breedName = finalizeName(breedName);
+        breedImg = breedImg + breed + "/images/random";
+        breedNames.push(breedName);
+        breedImgsSrcs.push(breedImg);
+      }
+      else {  // if the breed has sub-breeds
+        // traverse the sub-breeds and add them to the list
+        for (let i = 0; i < allBreedsObj[breed].length; ++i) {
+          const subBreed = allBreedsObj[breed][i];
+          breedName = capitalizeFirst(subBreed)
+            + " "
+            + capitalizeFirst(breed);
+          breedName = finalizeName(breedName);
+          breedImg = "https://dog.ceo/api/breed/" + breed + "/" + allBreedsObj[breed][i] + "/images/random";
+          breedNames.push(breedName);
+          breedImgsSrcs.push(breedImg);
+        }
+      }
+    }
+
+    for (let i = 0; i < breedNames.length; ++i) {
+      const breedName = breedNames[i];
+      const breedImg = (await
+          (await fetch(breedImgsSrcs[i])
+          ).json())
+        .message;
+      const breedInfo = "Lorem ipsum";
+      const card = createCard(breedImg, breedName, breedInfo);
+      row.append(card);
+    }
+}
+
+createAllCards();
