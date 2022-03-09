7367fc118b56a39a4aa99f5b0a45b0ba80790982
daliaramos <dalia.ramoshernandez@gmail.com>
2022-03-08 16:49:54 -0800

    added promise all array to not use many await

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
+const breedNames = [];
+const breedImgsSrcs = [];
+const subBreeds = [];
+
+const getResponse = async () => {
+  const allBreedsResponse = await fetch("https://dog.ceo/api/breeds/list/all");
+  const allBreedsObj = (await allBreedsResponse.json()).message;
+  return allBreedsObj;
+};
+
+getResponse().then((data) => {
+  getAllBreeds(breedNames, breedImgsSrcs, subBreeds, data);
+});
+
 const row = document.querySelector(".row");
+const all = document.querySelector("#all");
+const random = document.querySelector("#rand");
 const userInput = document.querySelector("#breed");
 userInput.addEventListener("input", search);
+random.addEventListener("click", randomImage);
 
 const dogDesc = async (breedName) => {
   //Wiki API
@@ -140,62 +157,40 @@ const createCard = (url, breedName, breedInfo) => {
 };
 
 //
-const createAllCards = async (input, cardCreated) => {
-  const allBreedsResponse = await fetch("https://dog.ceo/api/breeds/list/all");
-  const allBreedsObj = (await allBreedsResponse.json()).message;
-  const breedNames = [];
-  const breedImgsSrcs = [];
-  const subBreeds = [];
 
-  getAllBreeds(breedNames, breedImgsSrcs, subBreeds, allBreedsObj);
-  console.log(breedNames);
+const createAllCards = async (input) => {
   if (breedNames.includes(input) === true) {
     disableInput();
-    // for (let i = 0; i < searchBreedNames.length; ++i) {
     const breedName = input;
-    input = input.split(" ");
-    const searchImgBreed = `${`https://dog.ceo/api/breed/${input[0].toLowerCase()}/images/random`}`;
-    const breedImage = (await (await fetch(searchImgBreed)).json()).message;
-
-    const breedInfo = await dogDesc(finalizeName(breedName));
-    const card = createCard(breedImage, breedName, breedInfo);
-    row.append(card);
-    enableInput();
-    //    }
+    const breedImage = getImage(input);
+    const breedInfo = dogDesc(finalizeName(breedName));
+    const [image, info] = await Promise.all([breedImage, breedInfo]);
+    appendCard(image.message, breedName, info);
   } else {
     const searchBreedNames = subBreeds.filter(
       (element) => element.breed === `${input}`
     );
-    console.log(searchBreedNames);
     if (searchBreedNames.length > 0) {
       disableInput();
       for (let i = 0; i < searchBreedNames.length; ++i) {
         const breedName = searchBreedNames[i].sub;
-        const breedImage = (
-          await (await fetch(searchBreedNames[i].images)).json()
-        ).message;
+        const breedImage = (await fetch(searchBreedNames[i].images)).json();
         let sub = fixSubNames(searchBreedNames[i].sub);
-        console.log(sub);
-        const breedInfo = await dogDesc(finalizeName(sub));
-        const card = createCard(breedImage, breedName, breedInfo);
-        row.append(card);
-        enableInput();
+        const breedInfo = dogDesc(finalizeName(sub));
+        const [image, info] = await Promise.all([breedImage, breedInfo]);
+        appendCard(image.message, breedName, info);
       }
     } else {
       const search = subBreeds.filter((element) => element.sub === `${input}`);
-
       if (search.length > 0) {
+        cardCreated = true;
         disableInput();
         const breedName = search[0].sub;
-        const breedImage = (await (await fetch(search[0].images)).json())
-          .message;
+        const breedImage = (await fetch(search[0].images)).json();
         let sub = fixSubNames(search[0].sub);
-        let breed = search[0].breed;
-        console.log(sub);
-        const breedInfo = await dogDesc(finalizeName(sub));
-        const card = createCard(breedImage, breedName, breedInfo);
-        row.append(card);
-        enableInput();
+        const breedInfo = dogDesc(finalizeName(sub));
+        const [image, info] = await Promise.all([breedImage, breedInfo]);
+        appendCard(image.message, breedName, info);
       }
     }
   }
@@ -234,4 +229,23 @@ function disableInput() {
 
 function enableInput() {
   userInput.disabled = false;
+}
+
+function appendCard(breedImage, breedName, breedInfo) {
+  const card = createCard(breedImage, breedName, breedInfo);
+  row.append(card);
+  enableInput();
+}
+function getImage(input) {
+  input = input.split(" ");
+  const searchImgBreed = `${`https://dog.ceo/api/breed/${input[0].toLowerCase()}/images/random`}`;
+
+  const breedImage = fetch(searchImgBreed).then((data) => {
+    const image = data.json();
+    return image;
+  });
+  return breedImage;
+}
+function randomImage() {
+  createAllCards();
 }
